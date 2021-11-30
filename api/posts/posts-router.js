e11156bc812b07d1944fcb6d9ac99d4b66fec757
Angelique Abacajan <ma_utsey@yahoo.com>
2021-11-30 16:01:27 -0500

    Implemented GET Id request that Returns the post object with the specified id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -10,7 +10,24 @@ router.get("/", (req, res) => {
         res.status(200).json(posts);
 
     }).catch(error => {
-        res.status(500).json({message: 'The posts information could not be retrieved'})
+        res.status(500).json( { message: "The posts information could not be retrieved" } )
         console.log(error) 
+    })
+})
+
+// GET	/api/posts/:id
+router.get("/:id", (req, res) => {
+    const  id  = req.params.id
+    Post.findById(id)
+    .then(post => {
+        if(!post){
+            res.status(404).json( { message: "The post with the specified ID does not exist" } )
+
+        } else {
+            res.status(500).json(post)
+        }
+
+    }).catch(error => {
+        res.status(500).json({ message: error.message })
     })
 })
