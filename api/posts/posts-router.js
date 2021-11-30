0e1819e81037fcf22c25e2f0073f05432fb7a6a1
Angelique Abacajan <ma_utsey@yahoo.com>
2021-11-30 15:44:32 -0500

    Implemented GET request, HTTP status code, and JSON message

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,16 @@
 // implement your posts router here
+const Post = require("./posts-model");
+const express = require("express");
+const router = express.Router();
+
+// [GET] /api/posts
+router.get("/", (req, res) => {
+    Post.find(req)
+    .then(posts => {
+        res.status(200).json(posts);
+
+    }).catch(error => {
+        res.status(500).json({message: 'The posts information could not be retrieved'})
+        console.log(error) 
+    })
+})
